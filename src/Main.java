9da455853bd53a9cb31f97cde16775d2c89002a0
FabioMijango <00156022@uca.edu.sv>
2025-04-01 10:44:25 -0600

    Adding menu to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Bienvenido al Crustaceo Cascarudo\nQue desea hacer?");
+            System.out.println("1. Agregar nueva reserva\n2. Mostrar reservas pendientes\n3. Mostrar reservas terminadas\n0. Salir");
+            System.out.print("Ingrese una opcion: ");
+            Integer opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Agregar nueva reserva");
+                    break;
+                case 2:
+                    System.out.println("Mostrar reservas pendientes");
+                    break;
+                case 3:
+                    System.out.println("Mostrar resevas terminadas");
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+
     }
 }
