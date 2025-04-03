81f02270b1710232f49fac7a42e9a590652a46ca
GabrielMerinoUCA <gabriel.merino13079@est.uca.edu.ni>
2025-04-03 20:26:36 +1300

    add: funcionalidad evento familiar en MenuEvento

--- src/menu/MenuEvento.java
+++ src/menu/MenuEvento.java
@@ -3,6 +3,7 @@ package menu;
 import model.Cumpleanios;
 import model.Evento;
 import model.EventoEmpresarial;
+import model.EventoFamiliar;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -12,15 +13,19 @@ import java.util.Scanner;
 public class MenuEvento {
     private static final Double PRECIO_CENA_EMPRESARIAL = 320.00;
     private static final Double PRECIO_ALMUERZO_EMPRESARIAL = 300.00;
+    private static final Double PRECIO_CENA_FAMILIAR = 200.00;
+    private static final Double PRECIO_ALMUERZO_FAMILIAR = 180.00;
 
     private static final String CODIGO_CENA_EMPRESARIAL = "ED-E-03";
     private static final String CODIGO_ALMUERZO_EMPRESARIAL = "EE-E-03";
+    private static final String CODIGO_ALMUERZO_FAMILIAR = "EE-E-02";
+    private static final String CODIGO_CENA_FAMILIAR = "FD-E-04";
 
     public static Evento createEvento(String horaReserva) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Tipo de evento:");
-        System.out.println("1. Cumpleaños\n2. Almuerzo Familiar\n3. Evento Empresarial\n4. Cena Familiar");
+        System.out.println("1. Cumpleaños\n2. Evento Familiar\n3. Evento Empresarial");
         Integer tipoEvento = Integer.parseInt(scanner.nextLine());
         Evento evento = null;
         String comida = "";
@@ -47,27 +52,24 @@ public class MenuEvento {
                 evento = new Cumpleanios("HBD-E-01", 380, nombre, edad, colorFavorito, cantidadPersonas, modalidad, anticipo, comida);
                 break;
             case 2:
-                System.out.println("Almuerzo Familiar");
-                System.out.println("Apellido de la familia: ");
-                String apellido = scanner.nextLine();
-                comida = MenuComida.selectComida("comidaNormal");
+                evento = createEventoFamiliar(horaReserva);
                 break;
             case 3:
                 evento = createEventoEmpresarial(horaReserva);
-                break;
-            case 4:
                 break;
             default:
                 System.out.println("Opcion no valida");
                 break;
         }
-
         return evento;
     }
 
     private static EventoEmpresarial createEventoEmpresarial(String horaReserva) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Cena Empresarial");
+        String codigo = "";
+        Double precio = 0.0;
+        System.out.println("Evento Empresarial");
+
         System.out.println("Anticipo: ");
         Double anticipo = scanner.nextDouble();
         scanner.nextLine();
@@ -76,8 +78,6 @@ public class MenuEvento {
         Integer cantidadPersonas = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Comida: ");
-        String comida = scanner.nextLine();
 
         System.out.println("Nombre de la empresa: ");
         String nombreEmpresa = scanner.nextLine();
@@ -88,11 +88,10 @@ public class MenuEvento {
         System.out.println("Codigo de vestimenta: ");
         String codigoVestimenta = scanner.nextLine();
 
+        String comida = MenuComida.selectComida("comidaEmpresarial");
+
         System.out.println("Postre: ");
         String postre = scanner.nextLine();
-
-        String codigo = "";
-        Double precio = 0.0;
 
         if(determinarCenaAlumerzo(horaReserva).equals("ALMUERZO")) {
             codigo = CODIGO_ALMUERZO_EMPRESARIAL;
@@ -106,6 +105,37 @@ public class MenuEvento {
                 codigo, precio, anticipo, cantidadPersonas, comida, nombreEmpresa, tematicaColor,
                 codigoVestimenta, postre
         );
+    }
+
+    private static EventoFamiliar createEventoFamiliar(String horaReserva) {
+        Scanner scanner = new Scanner(System.in);
+        Double precio = 0.0;
+        String codigo = "";
+        System.out.println("Evento Familiar");
+
+        System.out.println("Apellido de la familia: ");
+        String apellido = scanner.nextLine();
+
+        System.out.println("Anticipo: ");
+        Double anticipo = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Cantidad de personas: ");
+        Integer cantidadPersonas = scanner.nextInt();
+        scanner.nextLine();
+
+        String comida = MenuComida.selectComida("comidaNormal");
+        String bebidas = MenuComida.selectComida("bebidaNormal");
+
+        if(determinarCenaAlumerzo(horaReserva).equals("ALMUERZO")) {
+            codigo = CODIGO_ALMUERZO_FAMILIAR;
+            precio = PRECIO_ALMUERZO_FAMILIAR;
+        } else if (determinarCenaAlumerzo(horaReserva).equals("CENA")) {
+            codigo = CODIGO_CENA_FAMILIAR;
+            precio = PRECIO_CENA_FAMILIAR;
+        }
+
+        return new EventoFamiliar(codigo, precio, anticipo, cantidadPersonas, comida, apellido, bebidas);
     }
 
     private static String determinarCenaAlumerzo(String horaReserva) {
